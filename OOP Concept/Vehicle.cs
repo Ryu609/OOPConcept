dc0e0f905f5d231b5f29d3541548480d94c733fa
Amardev Jonauth <m.joynauth@gmail.com>
2015-10-23 22:56:43 +0400

    FirstCommit OOP

---
+++ OOP Concept/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Concept
+{
+    class Vehicle
+    {
+        public string VehicleNo { get; set; }
+    }
+}
