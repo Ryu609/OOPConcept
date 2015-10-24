67727e2ea031e62990c0a5c02ee210d0c85c9158
Amardev Jonauth <m.joynauth@gmail.com>
2015-10-24 14:19:00 +0400

    Encapsulation Examples

--- OOP Concept/Program.cs
+++ OOP Concept/Program.cs
@@ -10,6 +10,24 @@ namespace OOP_Concept
     {
         static void Main(string[] args)
         {
+            //Example 1
+            var newCustomer = new Customer();
+
+            newCustomer.CustomerCode = "123";
+            newCustomer.CustomerName = "Mayur";
+            //call only the add method instead of call Add(), Validate() and createdbobject
+            //Code is simplified
+            newCustomer.Add();
+
+            //Example 2
+            var newReservation = new Reservation();
+
+            newReservation.RoomNumber = "23";
+            newReservation.RoomType = "Suite";
+            newReservation.Duration = 2;
+
+            newReservation.ReserverRoom();
+
         }
     }
 }
