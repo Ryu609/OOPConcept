60a3ef4298c64274ea3012fa1b0d0c34be193dc3
Amardev Jonauth <m.joynauth@gmail.com>
2015-10-24 20:10:36 +0400

    Polymorphism + Operator Overloading

---
+++ OOP Concept/Polymorphism.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOP_Concept
+{
+    public class Shape
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public virtual void Draw()
+        {
+            Console.WriteLine("Performing base class drawing tasks");
+        }
+    }
+
+    internal class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a circle");
+            base.Draw();
+        }
+    }
+
+    internal class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a rectangle");
+            base.Draw();
+        }
+    }
+}
