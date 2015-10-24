ea5a70cf778cd2a6241f45d75c969cc6276bec18
Amardev Jonauth <m.joynauth@gmail.com>
2015-10-24 14:19:29 +0400

    Encapsulation classes

---
+++ OOP Concept/Encapsulation.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Concept
+{
+    #region Encapsulation Example one
+    public class Customer
+    {
+
+        public string CustomerCode = "";
+        public string CustomerName = "";
+
+        public void Add()
+        {
+            // Add to database
+            Validate();
+            CreateDBObjects();
+
+            Console.WriteLine("if Validation and CreateDBObject is successful, the CustomerCode and CustomerName will be Inserted in the DB");
+            Console.ReadLine();
+
+        }
+
+        //set as private [Encapsulation] to decrease complexity
+        private bool Validate()
+        {
+            //Validation of Data goes here
+            //return true or false
+            return true;
+        }
+        //set as private [Encapsulation] to decrease complexity
+        private bool CreateDBObjects()
+        {
+            //create db connections
+            //return true or false
+            return true;
+        }
+    }
+    #endregion
+
+    #region Encapsulation Example Two
+    public class Reservation
+    {
+        public string RoomNumber { get; set; }
+        public string RoomType { get; set; }
+        public int Duration { get; set; }
+
+        public void ReserverRoom()
+        {
+            //Reserve room depending on value of IsAvailable
+           IsAvailable();
+        }
+
+        private bool IsAvailable()
+        {
+            //Verify if room is available
+            return true;
+
+        }
+    }
+
+
+    #endregion
+}
